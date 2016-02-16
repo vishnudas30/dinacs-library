728f23bee1fbdd5a003543697482cf2d91cb6780
datta <datta@datta-PC>
2016-02-16 01:12:40 -0800

    commiting Book.java

---
+++ dinacslibrary-portlet/docroot/WEB-INF/src/com/dinacs/Book.java
@@ -0,0 +1,37 @@
+package com.dinacs;
+
+public class Book {
+	
+	
+	private String bookTitle;
+    private String author;
+    private int bookId;
+    private String bookType;
+	public String getBookTitle() {
+		return bookTitle;
+	}
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookType() {
+		return bookType;
+	}
+	public void setBookType(String bookType) {
+		this.bookType = bookType;
+	}
+
+    
+
+}
